a685306ab6043be0a04b24d79b73b91ccf39f38b
Cauê Aguiar <caueaguiar96@hotmail.com>
2021-04-29 07:41:46 -0300

    <Added Working With Four Verbs Project>

---
+++ 03_RestWithASPNETUdemy_WorkingWithFourVerbs/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            
+
+        }
+
+    }
+}
